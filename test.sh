510a6a34aaa5fddd2b7c79da82c09ffbf3464879
sandeepmvd18 <sandeepmvd18@outlook.com>
2025-03-16 22:01:21 +0530

    testing

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Creating a directory
+mkdir MyDirectory
+
+# Moving into the directory
+cd MyDirectory
+
+# Creating a text file
+echo "Hello, World!" > MyFile.txt
+
+# Displaying the contents of the file
+cat MyFile.txt
+
